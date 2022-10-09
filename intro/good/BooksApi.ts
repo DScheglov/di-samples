9934ed6ff213df7ec9b0ae6df9bd2fa91faf3aa7
Dmitry Scheglov <dmitry.scheglov@gmail.com>
2022-10-09 11:37:05 +0200

    [dsch] intro/good: minor refactoring

--- intro/good/BooksApi.ts
+++ intro/good/BooksApi.ts
@@ -1,31 +1,36 @@
 import { isRequestError } from '../common/RequestError';
 import {
-  CreatePayload, CreateResponse, GetAllResponse, GetByIdResponse,
+  CreatePayload, CreateResponse, GetAllResponse, GetByIdResponse, ServerResponse,
 } from './Books.ServerTypes';
 import type { IBooksApi, CreateBookData, Book } from './IBooksApi';
 import type { IHttpClient } from './IHttpClient';
 
-const readDataFromBody = <T>(body: { data: T }) => body.data;
+const readDataFromBody = <T>(body: ServerResponse<T>) => body.data;
 
 export default class BooksApi implements IBooksApi {
   constructor(private readonly http: Pick<IHttpClient, 'get' | 'post'>) {}
 
   getAll(): Promise<Book[]> {
-    return this.http.get<GetAllResponse>('/books').then(readDataFromBody);
+    return this
+      .http
+      .get<GetAllResponse>('/books')
+      .then(readDataFromBody);
   }
 
   getById(bookId: string): Promise<Book | null> {
-    return this.http.get<GetByIdResponse>(`/books/${bookId}`).then(
-      readDataFromBody,
-      error => (
-        isRequestError(error) && error.isNotFound
-          ? null
-          : Promise.reject(error)
-      ),
-    );
+    return this
+      .http
+      .get<GetByIdResponse>(`/books/${bookId}`)
+      .then(readDataFromBody)
+      .catch(error => (
+        isRequestError(error) && error.isNotFound ? null : Promise.reject(error)
+      ));
   }
 
   create(bookData: CreateBookData): Promise<Book> {
-    return this.http.post<CreateResponse, CreatePayload>('/books', bookData).then(readDataFromBody);
+    return this
+      .http
+      .post<CreateResponse, CreatePayload>('/books', bookData)
+      .then(readDataFromBody);
   }
 }
