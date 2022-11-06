26e2835847b29016ca4db90f64bee983735b3e86
Dmitry Scheglov <dmitry.scheglov@gmail.com>
2022-11-06 14:22:43 +0100

    [dsch] refactoring good: BooksApi - making helpers as static private methods

--- intro/good/BooksApi.ts
+++ intro/good/BooksApi.ts
@@ -1,21 +1,32 @@
 import {
-  CreatePayload, CreateResponse, GetAllResponse, GetByIdResponse,
+  CreatePayload, CreateResponse, GetAllResponse, GetByIdResponse, ServerResponse,
 } from './Books.ServerTypes';
-import { hasStatus, readDataFromBody, rejectError } from './helpers';
 import type { IBooksApi, CreateBookData, Book } from './IBooksApi';
-import type { IHttpClient } from './IHttpClient';
+import type { IHttpClient, Response } from './IHttpClient';
 
 export default class BooksApi implements IBooksApi {
   constructor(private readonly http: Pick<IHttpClient, 'get' | 'post'>) {}
+
+  private static readDataFromBody <T>({ body }: Response<ServerResponse<T>>) {
+    return body.data;
+  }
+
+  private static hasStatus({ status }: Response<any>, expectedStatus: number) {
+    return status === expectedStatus;
+  }
+
+  private static rejectError(message: string, { status, statusText }: Response<any>) {
+    return Promise.reject(new Error(`${message}: ${status} - ${statusText}`));
+  }
 
   getAll(): Promise<Book[]> {
     return this
       .http
       .get<GetAllResponse>('/books')
       .then(response => (
-        hasStatus(response, 200)
-          ? readDataFromBody(response)
-          : rejectError('Failed to fetch Books', response)
+        BooksApi.hasStatus(response, 200)
+          ? BooksApi.readDataFromBody(response)
+          : BooksApi.rejectError('Failed to fetch Books', response)
       ));
   }
 
@@ -24,9 +35,9 @@ export default class BooksApi implements IBooksApi {
       .http
       .get<GetByIdResponse>(`/books/${bookId}`)
       .then(response => (
-        hasStatus(response, 200) ? readDataFromBody(response) :
-        hasStatus(response, 404) ? null :
-        rejectError(`Failed to fetch Book<${bookId}>`, response)
+        BooksApi.hasStatus(response, 200) ? BooksApi.readDataFromBody(response) :
+        BooksApi.hasStatus(response, 404) ? null :
+        BooksApi.rejectError(`Failed to fetch Book<${bookId}>`, response)
       ));
   }
 
@@ -35,9 +46,9 @@ export default class BooksApi implements IBooksApi {
       .http
       .post<CreateResponse, CreatePayload>('/books', bookData)
       .then(response => (
-        hasStatus(response, 201)
-          ? readDataFromBody(response)
-          : rejectError(`Failed to Create Book<${bookData.isbn.join(', ')}>`, response)
+        BooksApi.hasStatus(response, 201)
+          ? BooksApi.readDataFromBody(response)
+          : BooksApi.rejectError(`Failed to Create Book<${bookData.isbn.join(', ')}>`, response)
       ));
   }
 }
